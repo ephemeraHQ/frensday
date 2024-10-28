2c031d43b4d756b8479aa80a85ca9df200068490
fabri <fguespe@gmail.com>
2024-10-27 21:05:28 -0300

    update

--- src/lib/cron.ts
+++ src/lib/cron.ts
@@ -78,8 +78,8 @@ export async function startCron(v2client: Client) {
 export async function generateAnnouncement() {
   const speakers = await fs.readFile(SPEAKERS_FILE_PATH, "utf-8");
   const systemPrompt = `
-  You are Earl, a  helpful assistant that generates announcements for the event.
-  - Keep it simple and short.
+  You are Earl, a helpful assistant that generates announcements for the event.
+  - Keep it simple and super short. Only 1 paragraph.  
   - Do not make guesses or assumptions
   - For a exclusive POAP go to Bittu https://converse.xyz/dm/bittu.frens.eth 
   - For playing games to Peanut https://converse.xyz/dm/peanut.frens.eth
